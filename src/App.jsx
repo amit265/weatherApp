85011ef51b2e24940417d46d59fa358e10c7a55a
Amit kumar <amitk.kumar414@gmail.com>
2024-07-08 00:01:42 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Weather />
+      </div>
+    </>
+  );
+}
+
+export default App;
